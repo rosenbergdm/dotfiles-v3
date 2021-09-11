e6079f4093e8c6efb7b0b84817ff17c7112fdc69
David Rosenberg <dmr@davidrosenberg.me>
2021-09-11 18:17:37 -0500

    added .macos script

--- .config/drvim/lua/core/builtins.lua
+++ .config/drvim/lua/core/builtins.lua
@@ -1,7 +1,25 @@
 local M = {}
-M.interface = {}
-M.builtins = {}
-M.plugin_cfg = {}
-M.plugin_mappings = {}
+-- M.interface = {}
+-- M.builtins = {}
+-- M.plugin_cfg = {}
+-- M.plugin_mappings = {}
+-- M.default_plugins = {}
 
-M.default_plugins = {}
+M.builtins = {}
+M.builtins.plugins = {
+    {"wbthomason/packer.nvim"}, {"neovim/nvim-lspconfig", opt = false},
+    {"folke/tokyonight.nvim", opt = false }, {"tamago324/nlsp-settings.nvim", opt = false}, {
+        "kyazdani42/nvim-tree.lua",
+        -- event = "BufWinOpen",
+        -- cmd = "NvimTreeToggle",
+        -- commit = "fd7f60e242205ea9efc9649101c81a07d5f458bb",
+        config = function() require("core.config.nvimtree").setup() end
+        -- disable = not lvim.builtin.nvimtree.active,
+    }
+}
+
+local use = drvim.packer.use
+
+for _, plugin in ipairs(M.builtins.plugins) do use(plugin) end
+
+return M
