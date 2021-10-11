88e805ba11772ec8526aad766e08a45717422337
David Rosenberg <dmr@davidrosenberg.me>
2021-10-11 05:13:31 -0500

    updated startup scripts

--- installer.sh
+++ installer.sh
@@ -6,7 +6,7 @@ declare -a base_install_linux=( llvm autotools-dev libtool
   libunibilium-dev libmsgpack-dev libtermkey-dev 
   ninja-build gettext libtool libtool-bin autoconf automake 
   cmake g++ pkg-config unzip curl doxygen git gpg apache2 libapache2-mod-wsgi
-  ccache)
+  ccache man-db )
 declare -a npm_packages=()
 declare -a ruby_packages=()
 declare -a python_packages=()
@@ -20,14 +20,17 @@ sudo apt install ${base_install_linux[@]}
 
 # rust
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
+source ~/.bashrc
 
 # go 
 cd ~
 mkdir -p src sys-setup
 cd sys-setup
 wget https://golang.org/dl/go1.17.2.linux-amd64.tar.gz
-sudo rm -rf /usr/local/go && tar -C /usr/local -xzf go*.tar.gz
-cd ~
+sudo rm -rf /usr/local/go && sudo tar -C /usr/local -xzf go*.tar.gz
+echo "export PATH=/usr/local/go/bin:\$PATH" >> ~/.bashrc 
+source ~/.bashrc
+cd 
 
 # python
 
@@ -37,12 +40,13 @@ cd ~
 # node
 # nvm
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.0/install.sh | bash
+source ~/.bashrc
 nvm install node --latest-npm
 
 # rvm 
 gpg2 --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3 7D2BAF1CF37B13E2069D6956105BD0E739499BDB
 curl -sSL https://get.rvm.io | bash -s stable
+source ~/.bashrc
 ruby_version="$(rvm list known | grep ruby-head  -B1 | head -n1 |perl -p -e 's/[\[|\]]//g')"
 rvm install --docs --rdoc --yard $ruby_version
-rvm 
 
