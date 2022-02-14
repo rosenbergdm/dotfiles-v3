a73c9304cb885b9520570bad2488a7ade348cd8b
David Rosenberg <dmr@davidrosenberg.me>
2022-02-13 20:36:45 -0600

    removed access token

--- .config/lvim/after/ftplugin/r.lua
+++ .config/lvim/after/ftplugin/r.lua
@@ -6,4 +6,10 @@ for _, lspserver in pairs(lspservers) do
   require("lspconfig")[lspserver].setup {}
 end
 
+-- local opts = {
+--   on_attach = require("lvm.lsp").common_on_attach,
+--   on_init = require("lvim.lsp").common_on_init,
+--   capabilities = require("lvim.lsp").common_capabilities(),
+-- }
+
 require("lvim.lsp.manager").setup "r_language_server"
